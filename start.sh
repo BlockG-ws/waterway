c088a3617461693b0fd341b214e6e3630e973d7d
草方块 <46253950+GrassBlock1@users.noreply.github.com>
2026-02-10 16:10:32 +0800

    chore: do not register by default [skip ci]

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ nohup /usr/bin/warp-svc > /app/warp.log &
 
 sleep 5
 
-warp-cli --accept-tos registration new
 warp-cli --accept-tos mode proxy
-warp-cli --accept-tos connect
+
 
 if [ -n "$SOCKS5_MODE" ]; then
     /app/gost -L tcp://:1080/127.0.0.1:40000 -L udp://:1080/127.0.0.1:40000
